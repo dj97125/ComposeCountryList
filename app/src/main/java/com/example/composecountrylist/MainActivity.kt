bcf209a193bb5d6b73711156cb7c2b46d6e51562
dj97125 <102561977+dj97125@users.noreply.github.com>
2023-06-30 09:11:58 -0700

    migrated gradle groovy to kotlin DSL

--- app/src/main/java/com/example/composecountrylist/MainActivity.kt
+++ app/src/main/java/com/example/composecountrylist/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.composecountrylist
 
-import androidx.appcompat.app.AppCompatActivity
+
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.danielcaballero.mynewcompose.ui.theme.MyNewComposeTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setContent {
+            MyNewComposeTheme {
+                Surface {
+                    modifier = Modifier.fillMaxSize(),
+                    color
+                }
+
+            }
+        }
         setContentView(R.layout.activity_main)
     }
 }
